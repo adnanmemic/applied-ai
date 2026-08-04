e6ec05a158c0a82a6c1378b05a761bcdf5e2a678
Adnan Memic <adnan.memic27@gmail.com>
2026-08-04 17:42:25 +0200

    feat: add first task (ex1) and tests

---
+++ aaip/ex1.py
@@ -0,0 +1,2 @@
+def hello_python_exercise(num_students, student_name="Adnan Memic"):
+    print(f"This course has {num_students} and one of those is {student_name}.")
---
+++ ex1_test.py
@@ -0,0 +1,4 @@
+from aaip import ex1
+
+ex1.hello_python_exercise(50)
+ex1.hello_python_exercise(25, "John Doe")
