b8473097c171af187f81c4600fc53b537bce20dc
Adnan Memic <adnan.memic27@gmail.com>
2026-08-19 12:26:45 +0200

    feat: add students to the list iteratively in ex2

--- aaip/ex2.py
+++ aaip/ex2.py
@@ -4,3 +4,26 @@ def get_students_from_csv(path):
     with open(path, "r", encoding="utf-8") as f:
         database = list(csv.DictReader(f))
         return database
+
+def add_student(database):
+    while True:
+        print("Enter the students name: ")
+        name = input()
+
+        if name == "":
+            return
+
+        print("Enter the students grade: ")
+        grade = input()
+
+        if not database:
+            new_id = 1
+        else:
+            id_list = [int(row['id']) for row in database]
+            new_id = max(id_list) + 1
+
+        database.append({
+            'id': str(new_id), 
+            'name': name,
+            'grade': grade
+        })
