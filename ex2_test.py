bf73dfb386dfb4f2d0b87ca44c3aa6fdd8ecf87b
Adnan Memic <adnan.memic27@gmail.com>
2026-08-23 22:58:15 +0200

    fix: improve error handling and input validation in ex2

--- ex2_test.py
+++ ex2_test.py
@@ -1,15 +1,22 @@
 from aaip import ex2
 
-studentslist = ex2.get_students_from_csv('aaip/data/students_list.csv')
-print(studentslist)
+try:
+    studentslist = ex2.get_students_from_csv('aaip/data/students_list.csv')
+    print(studentslist)
+except OSError:
+    print("Error: Could not open the file!")
 
 ex2.add_student(studentslist)
 print(studentslist)
 
 ex2.search_student(studentslist, student_id=2)
 ex2.search_student(studentslist, name="someone")
+ex2.search_student(studentslist, name="anybody")
 
 ex2.change_grade(studentslist, 2, 3)
 print(studentslist)
 
-ex2.store_students_into_csv(studentslist, 'aaip/data/students_list.csv')
+try: 
+    ex2.store_students_into_csv(studentslist, 'aaip/data/students_list.csv')
+except OSError:
+    print("Error: Could not open the file!")
