41fb7664d4a922a9d93dc26342e36d8ecdf0c216
Adnan Memic <adnan.memic27@gmail.com>
2026-08-24 18:26:48 +0200

    feat: add input validation to person constructor in ex3

--- aaip/ex3.py
+++ aaip/ex3.py
@@ -1,5 +1,14 @@
 class Person:
     def __init__(self, name: str, age: int, address: str):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string!")
+
+        if not isinstance(age, int):
+            raise TypeError("Age must be an integer!")
+
+        if not isinstance(address, str):
+            raise TypeError("Address must be a string!")
+
         self.name = name
         self.age = age
         self.address = address
