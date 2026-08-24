d13477f9d78ea187eeed04ed72d6e1625d520625
Adnan Memic <adnan.memic27@gmail.com>
2026-08-24 20:31:28 +0200

    test: add tests for the factory method in ex3

--- test_ex3.py
+++ test_ex3.py
@@ -20,3 +20,18 @@ class TestPerson(unittest.TestCase):
     def test_invalid_address_type(self):
         with self.assertRaises(TypeError):
             ex3.Person("John", 26, 3)
+    
+    def test_from_string_attribute_values(self):
+        person = ex3.Person.from_string("John, 26, Somestreet 50")
+        self.assertEqual(person.name, "John")
+        self.assertEqual(person.age, 26)
+        self.assertEqual(person.address, "Somestreet 50")
+    
+    def test_from_string_returns_person(self):
+        person = ex3.Person.from_string("John, 26, Somestreet 50")
+        self.assertIsInstance(person, ex3.Person)
+    
+    def test_from_string_invalid_type(self):
+        with self.assertRaises(TypeError):
+            ex3.Person.from_string(5)
+
