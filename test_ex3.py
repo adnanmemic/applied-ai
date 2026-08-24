2e54d7c56daca3927f994fc7f2dbd429c879f2c9
Adnan Memic <adnan.memic27@gmail.com>
2026-08-24 20:44:32 +0200

    test: remove setUp method in ex3

--- test_ex3.py
+++ test_ex3.py
@@ -3,11 +3,9 @@ from aaip import ex3
 
 class TestPerson(unittest.TestCase):
 
-    def setUp(self):
-        self.person1 = ex3.Person("John", 30, "Somestreet 50")
-
     def test_greet_person(self):
-        self.assertEqual(self.person1.greet(), "Greetings to John")
+        person1 = ex3.Person("John", 30, "Somestreet 50")
+        self.assertEqual(person1.greet(), "Greetings to John")
 
     def test_invalid_name_type(self):
         with self.assertRaises(TypeError):
