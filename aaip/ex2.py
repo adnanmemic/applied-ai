45240aeda3d6f32a1780f7d8dbcae52677f3c8b7
Adnan Memic <adnan.memic27@gmail.com>
2026-08-25 17:44:04 +0200

    style: fix formatting in ex1, ex2, ex3, and ex8 and its tests

--- aaip/ex2.py
+++ aaip/ex2.py
@@ -1,15 +1,17 @@
 import csv
 
+
 def get_students_from_csv(path: str) -> list[dict[str, str]] | None:
-        with open(path, "r", encoding="utf-8", newline="") as f:
-            database = list(csv.DictReader(f))
-            return database
+    with open(path, "r", encoding="utf-8", newline="") as f:
+        database = list(csv.DictReader(f))
+        return database
+
 
 def add_student(database: list[dict[str, str]]) -> None:
     while True:
         print("Enter the students name: ")
         name = input()
-        if not name.strip(): # stop adding students when no name was entered
+        if not name.strip():  # stop adding students when no name was entered
             return
 
         try:
@@ -29,15 +31,12 @@ def add_student(database: list[dict[str, str]]) -> None:
             id_list = [int(row["id"]) for row in database]
             new_id = max(id_list) + 1
 
-        database.append({
-            "id": str(new_id), 
-            "name": name,
-            "grade": str(grade)
-        })
+        database.append({"id": str(new_id), "name": name, "grade": str(grade)})
+
 
 def search_student(
-    database: list[dict[str, str]], 
-    student_id: int | None = None, 
+    database: list[dict[str, str]],
+    student_id: int | None = None,
     name: str | None = None,
 ) -> dict[str, str] | None:
 
@@ -54,28 +53,35 @@ def search_student(
         print("Search for ID:")
         for row in database:
             if int(row["id"]) == student_id:
-                print(f"ID: {row["id"]} \nName: {row["name"]} \nGrade: {row["grade"]} \n")
+                print(
+                    f"ID: {row['id']} \nName: {row['name']} \nGrade: {row['grade']} \n"
+                )
                 return row
 
     elif name is not None:
         if not isinstance(name, str):
             raise TypeError("Name must be a string!")
 
-        if not name.strip(): 
+        if not name.strip():
             raise ValueError("Name can not be empty!")
 
         print("Search for name:")
         for row in database:
             if row["name"] == name:
-                print(f"ID: {row["id"]} \nName: {row["name"]} \nGrade: {row["grade"]} \n")
+                print(
+                    f"ID: {row['id']} \nName: {row['name']} \nGrade: {row['grade']} \n"
+                )
                 return row
 
     else:
         raise ValueError("Either ID or Name must be provided!")
-    
+
     return None
 
-def change_grade(database: list[dict[str, str]], student_id: int, new_grade: int) -> None:                
+
+def change_grade(
+    database: list[dict[str, str]], student_id: int, new_grade: int
+) -> None:
     if not database:
         raise ValueError("Database is empty!")
 
@@ -95,6 +101,7 @@ def change_grade(database: list[dict[str, str]], student_id: int, new_grade: int
         if int(student["id"]) == student_id:
             student["grade"] = str(new_grade)
             break
+
 
 def store_students_into_csv(database: list[dict[str, str]], path: str) -> None:
     if not database:
--- test_ex3.py
+++ test_ex3.py
@@ -1,8 +1,9 @@
 import unittest
+
 from aaip import ex3
 
-class TestPerson(unittest.TestCase):
 
+class TestPerson(unittest.TestCase):
     def test_greet_person(self):
         person = ex3.Person("John", 30, "Somestreet 50")
         self.assertEqual(person.greet(), "Greetings to John")
@@ -18,7 +19,7 @@ class TestPerson(unittest.TestCase):
     def test_invalid_address_type(self):
         with self.assertRaises(TypeError):
             ex3.Person("John", 26, 3)
-    
+
     def test_greet_student(self):
         student = ex3.Student("John", 30, "Somestreet 50", "Uni")
         self.assertEqual(student.greet(), "Greetings to John and his university: Uni")
@@ -26,18 +27,17 @@ class TestPerson(unittest.TestCase):
     def test_invalid_university_type(self):
         with self.assertRaises(TypeError):
             ex3.Student("John", 26, "Anystreet 3", 5)
-    
+
     def test_from_string_attribute_values(self):
         person = ex3.Person.from_string("John, 26, Somestreet 50")
         self.assertEqual(person.name, "John")
         self.assertEqual(person.age, 26)
         self.assertEqual(person.address, "Somestreet 50")
-    
+
     def test_from_string_returns_person(self):
         person = ex3.Person.from_string("John, 26, Somestreet 50")
         self.assertIsInstance(person, ex3.Person)
-    
+
     def test_from_string_invalid_type(self):
         with self.assertRaises(TypeError):
             ex3.Person.from_string(5)
-
