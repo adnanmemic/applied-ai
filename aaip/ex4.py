1399a0647fbb60ee682ecbe9b8d3e580b38d435e
Adnan Memic <adnan.memic27@gmail.com>
2026-08-26 17:03:51 +0200

    feat: add complex number addition in ex4

--- aaip/ex4.py
+++ aaip/ex4.py
@@ -2,3 +2,8 @@ class ComplexNumber:
     def __init__(self, real, imaginary):
         self.real = real
         self.imaginary = imaginary
+
+    def __add__(self, other):
+        real = self.real + other.real
+        imaginary = self.imaginary + other.imaginary
+        return ComplexNumber(real, imaginary)
