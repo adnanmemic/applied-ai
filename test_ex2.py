75a6f0c847ae3447f60b1f2c007c70dd32dcf297
Adnan Memic <adnan.memic27@gmail.com>
2026-08-26 22:31:43 +0200

    test: change tests for ex2 to unittests

--- test_ex2.py
+++ test_ex2.py
@@ -1,22 +1,67 @@
+import csv
+import unittest
+from pathlib import Path
+from tempfile import TemporaryDirectory
+from unittest.mock import patch
+
 from aaip import ex2
 
-try:
-    studentslist = ex2.get_students_from_csv("aaip/data/students_list.csv")
-    print(studentslist)
-except OSError:
-    print("Error: Could not open the file!")
 
-ex2.add_student(studentslist)
-print(studentslist)
+class TestStudentDatabase(unittest.TestCase):
+    def setUp(self):
+        self.database = [
+            {"id": "1", "name": "John", "grade": "5"},
+            {"id": "2", "name": "Trevor", "grade": "4"},
+            {"id": "3", "name": "Sergei", "grade": "2"},
+            {"id": "4", "name": "Ivan", "grade": "1"},
+        ]
 
-ex2.search_student(studentslist, student_id=2)
-ex2.search_student(studentslist, name="someone")
-ex2.search_student(studentslist, name="anybody")
+    def test_get_students_from_csv(self):
+        content = self.database
 
-ex2.change_grade(studentslist, 2, 3)
-print(studentslist)
+        with TemporaryDirectory() as tmpdir:
+            path_name = str(Path(tmpdir) / "data.csv")
 
-try:
-    ex2.store_students_into_csv(studentslist, "aaip/data/students_list.csv")
-except OSError:
-    print("Error: Could not open the file!")
+            with open(path_name, "w", encoding="utf-8", newline="") as file:
+                fieldnames = ["id", "name", "grade"]
+                csv_writer = csv.DictWriter(file, fieldnames)
+                csv_writer.writeheader()
+                csv_writer.writerows(content)
+
+            database = ex2.get_students_from_csv(path_name)
+
+            self.assertEqual(database, content)
+
+    def test_add_student(self):
+        with patch("builtins.input", side_effect=["Bob", "3", ""]):
+            ex2.add_student(self.database)
+
+        self.assertEqual(self.database[-1], {"id": "5", "name": "Bob", "grade": "3"})
+
+    def test_search_student(self):
+        self.assertEqual(
+            ex2.search_student(self.database, student_id=3),
+            {"id": "3", "name": "Sergei", "grade": "2"},
+        )
+
+        self.assertEqual(
+            ex2.search_student(self.database, name="Trevor"),
+            {"id": "2", "name": "Trevor", "grade": "4"},
+        )
+
+    def test_change_grade(self):
+        ex2.change_grade(self.database, 1, 2)
+
+        self.assertEqual(self.database[0], {"id": "1", "name": "John", "grade": "2"})
+
+    def test_store_students_into_csv(self):
+        file_content = []
+
+        with TemporaryDirectory() as tmpdir:
+            path_name = str(Path(tmpdir) / "data.csv")
+            ex2.store_students_into_csv(self.database, path_name)
+
+            with open(path_name, "r", encoding="utf-8", newline="") as file:
+                file_content = list(csv.DictReader(file))
+
+        self.assertEqual(file_content, self.database)
