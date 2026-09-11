44e7d39e746688f6e61e2eb37553e763ca83a3e7
Adnan Memic <adnan.memic27@gmail.com>
2026-09-11 22:58:21 +0200

    fix: print in value error block in ex6

--- aaip/ex6.py
+++ aaip/ex6.py
@@ -71,7 +71,7 @@ def main() -> None:
         print(f"Error: file not found: {file_path}", file=sys.stderr)
         sys.exit(1)
     except ValueError:
-        print(f"Error: word cannot be empty or whispace: {file_path}", file=sys.stderr)
+        print("Error: word cannot be empty or whispace", file=sys.stderr)
         sys.exit(2)
 
 
