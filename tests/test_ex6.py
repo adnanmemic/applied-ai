cace5b9a08db12ef4b5df689c68026aa4282d736
Adnan Memic <adnan.memic27@gmail.com>
2026-09-12 23:20:54 +0200

    test: add missing tests in ex6

--- tests/test_ex6.py
+++ tests/test_ex6.py
@@ -1,11 +1,13 @@
 import os
+import sys
 import unittest
+from unittest.mock import patch
 from tempfile import NamedTemporaryFile
 
 from aaip import ex6
 
 
-class TestCommanLineTool(unittest.TestCase):
+class TestCommandLineTool(unittest.TestCase):
     def setUp(self):
         # delete = false doesn't delete the file when being closed
         with NamedTemporaryFile(mode="w", encoding="utf-8", delete=False) as file:
@@ -32,3 +34,65 @@ class TestCommanLineTool(unittest.TestCase):
             ex6.search_word(self.file_path, "")
         with self.assertRaises(ValueError):
             ex6.search_word(self.file_path, "   ")
+
+    @patch("builtins.print")
+    @patch("sys.argv", new_callable=list)
+    def test_main_count_words(self, mock_argv, mock_print):
+        mock_argv.extend(["aaip/ex6.py", self.file_path, "count_words"])
+        ex6.main()
+        mock_print.assert_called_once_with("Words: ", 7)
+
+    @patch("builtins.print")
+    @patch("sys.argv", new_callable=list)
+    def test_main_count_lines(self, mock_argv, mock_print):
+        mock_argv.extend(["aaip/ex6.py", self.file_path, "count_lines"])
+        ex6.main()
+        mock_print.assert_called_once_with("Lines: ", 2)
+
+    @patch("builtins.print")
+    @patch("sys.argv", new_callable=list)
+    def test_main_search_word(self, mock_argv, mock_print):
+        mock_argv.extend(["aaip/ex6.py", self.file_path, "search_word", "sentence"])
+        ex6.main()
+        mock_print.assert_called_once_with("This is a sentence")
+
+    @patch("sys.argv", new_callable=list)
+    def test_main_missing_second_argument(self, mock_argv):
+        mock_argv.extend(["aaip/ex6.py", self.file_path])
+        with self.assertRaises(SystemExit) as context:
+            ex6.main()
+
+        self.assertEqual(context.exception.code, 2)
+
+    @patch("sys.argv", new_callable=list)
+    def test_main_missing_third_argument(self, mock_argv):
+        mock_argv.extend(["aaip/ex6.py", self.file_path, "search_word"])
+        with self.assertRaises(SystemExit) as context:
+            ex6.main()
+
+        self.assertEqual(context.exception.code, 2)
+
+    @patch("sys.argv", new_callable=list)
+    def test_main_wrong_action(self, mock_argv):
+        mock_argv.extend(["aaip/ex6.py", self.file_path, "count_anything"])
+        with self.assertRaises(SystemExit) as context:
+            ex6.main()
+
+        self.assertEqual(context.exception.code, 2)
+
+    @patch("sys.argv", new_callable=list)
+    def test_main_file_not_found(self, mock_argv):
+        non_existing_file = "hello"
+        mock_argv.extend(["aaip/ex6.py", non_existing_file, "count_words"])
+        with self.assertRaises(SystemExit) as context:
+            ex6.main()
+
+        self.assertEqual(context.exception.code, 1)
+
+    @patch("sys.argv", new_callable=list)
+    def test_main_word_is_empty_or_whitespace(self, mock_argv):
+        mock_argv.extend(["aaip/ex6.py", self.file_path, "search_word", " "])
+        with self.assertRaises(SystemExit) as context:
+            ex6.main()
+
+        self.assertEqual(context.exception.code, 2)
