03b84b6b728c86ab8365cb1133086ec6cce2e20b
Adnan Memic <adnan.memic27@gmail.com>
2026-09-17 01:46:10 +0200

    feat: add exception handling to ex1

--- aaip/ex1.py
+++ aaip/ex1.py
@@ -1,2 +1,12 @@
 def hello_python_exercise(num_students: int, student_name: str = "Adnan Memic") -> None:
+    if not isinstance(num_students, int):
+        raise TypeError("num_students must be an integer.")
+    if num_students < 0:
+        raise ValueError("num_students cannot be negative.")
+
+    if not isinstance(student_name, str):
+        raise TypeError("student_name must be a string.")
+    if not student_name.strip():
+        raise ValueError("student_name cannot be empty.")
+
     print(f"This course has {num_students} and one of those is {student_name}.")
